19da6b29af92f75f71f0fccda1c9623550f54056
Tony Burks <tonyburks@me.com>
2022-10-05 22:52:50 -0500

    updated footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,66 @@
-import { Box, BoxProps, Container } from '@chakra-ui/react'
+import { Stack, Text, useColorModeValue, VisuallyHidden, Box, BoxProps, Container, chakra, } from '@chakra-ui/react'
 import * as React from 'react'
+import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { ReactNode } from 'react';
+import Logo from '../Navbar/Logo'
 
 export default function Footer(props: BoxProps) {
-  return (
-    <Box as="footer" role="contentinfo" bg="bg-accent" {...props}>
-      <Container>
+  const SocialButton = ({
+    children,
+    label,
+    href,
+  }: {
+    children: ReactNode;
+    label: string;
+    href: string;
+  }) => {
+    return (
+      <chakra.button
+        bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+        rounded={'full'}
+        w={8}
+        h={8}
+        cursor={'pointer'}
+        as={'a'}
+        href={href}
+        display={'inline-flex'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        transition={'background 0.3s ease'}
+        _hover={{
+          bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+        }}>
+        <VisuallyHidden>{label}</VisuallyHidden>
+        {children}
+      </chakra.button>
+    );
+  };
 
+
+  const date = new Date()
+  return (
+    <Box as="footer" role="contentinfo" bg={useColorModeValue('gray.50', 'gray.900')} color={useColorModeValue('gray.700', 'gray.200')} {...props}>
+      <Container
+      as={Stack}
+      maxW={'6xl'}
+      py={4}
+      direction={{ base: 'column', md: 'row' }}
+      spacing={4}
+      justify={{ base: 'center', md: 'space-between' }}
+      align={{ base: 'center', md: 'center' }}>
+        <Logo />
+        <Text>© {date.getFullYear()} Sigma Pi Hub. All rights reserved</Text>
+        <Stack direction={'row'} spacing={6}>
+          <SocialButton label={'Twitter'} href={'#'}>
+            <FaTwitter />
+          </SocialButton>
+          <SocialButton label={'YouTube'} href={'#'}>
+            <FaYoutube />
+          </SocialButton>
+          <SocialButton label={'Instagram'} href={'#'}>
+            <FaInstagram />
+          </SocialButton>
+        </Stack>
       </Container>
     </Box>
   )
